b4ea5cdc8ab1d9c2fa9ca29d10dabc58a316c9d1
Ilya Kluyzhev <ilia.kluyzhev.2498@gmail.com>
2025-04-07 15:38:15 +0300

    Educational changes

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,34 @@
 from rest_framework import viewsets
-
 from .models import Achievement, Cat, User
-
+from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from rest_framework.throttling import AnonRateThrottle, ScopedRateThrottle
+from .throttling import WorkingHoursRateThrottle
+from rest_framework.pagination import LimitOffsetPagination
+from .pagination import CatsPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    pagination_class = None
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',) 
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+    def get_permissions(self):
+        # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернём обновлённый перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
